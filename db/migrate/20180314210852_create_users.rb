4772aedc0c072e9a17245f482fbbe6f9a3c15ab8
Ian Marshall <imarshall14@gmail.com>
2021-01-31 14:04:19 -0800

    commit tree 794208fdb07e268a3edf1ea19ce3d9f7a4b7117d

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest #this salts and hashes using bcrypt
+    end
   end
-  
+
   def down
+    drop_table :users
   end
-end
+end 
